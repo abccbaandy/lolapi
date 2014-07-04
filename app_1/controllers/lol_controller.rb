e1b46f68f19477b8ccff4ec6b72fab0fdf1acd1c
abccbaandy <abccbaandy@vm-0.abccbaandy.koding.kd.io>
2014-07-04 07:05:43 +0000

    test2

--- app_1/controllers/lol_controller.rb
+++ app_1/controllers/lol_controller.rb
@@ -86,7 +86,171 @@ class LolController < ApplicationController
     end
     def index
     end
-    def new
+    def show
+        key = "edadd4f4-b535-4c14-b2f8-b667e4a59e8c"
+        
+        session[:guest] = params[:id]
+        #summonerId = apiGetSummonerId(session[:guest], key)
+        parsedSummonerId = apiGetSummonerId(session[:guest], key)
+        if(parsedSummonerId.has_key?('errCode'))
+            session[:errCode] = parsedSummonerId['errCode']
+            session[:api] = parsedSummonerId['api']
+            redirect_to '/lol/'
+            return
+        end
+        summonerId = parsedSummonerId[session[:guest]]['id'].to_s
+        
+        parsedSummonerLeagueEntries = apiGetSummonerLeagueEntries(summonerId, key)
+        if(parsedSummonerLeagueEntries.has_key?('errCode'))
+            session[:errCode] = parsedSummonerLeagueEntries['errCode']
+            session[:api] = parsedSummonerLeagueEntries['api']
+            redirect_to '/lol/'
+            return
+        end
+        
+        @summonerDivision = parsedSummonerLeagueEntries[summonerId][0]['entries'][0]['division']
+        @summonerTier = parsedSummonerLeagueEntries[summonerId][0]['tier']
+        @summonerLeaguePoints = parsedSummonerLeagueEntries[summonerId][0]['entries'][0]['leaguePoints']
+        parsedSummonerRunes = apiGetSummonerRunes(summonerId, key)
+        
+        #find current rune page
+        @currentRunePage = -1
+        parsedSummonerRunes[summonerId]['pages'].each_with_index do |page, index|
+            if(page['current']==true)
+                @currentRunePage = index
+                break
+            end
+        end
+        
+        #show detail of current rune page 
+        @runes = Array.new
+        @runesColor = Hash.new
+        
+        if((@currentRunePage != -1) && (!parsedSummonerRunes[summonerId]['pages'][@currentRunePage]['slots'].nil?))
+            parsedSummonerRunes[summonerId]['pages'][@currentRunePage]['slots'].each_with_index do |slot, index|
+                
+                if(Rune.where( :runeId => slot['runeId'].to_s).exists?)
+                    runeSql = Rune.where( :runeId => slot['runeId'].to_s).first
+                    runeData = Hash.new
+                    runeData['description'] = runeSql.runeDescription
+                    runeData['rune'] = Hash.new
+                    runeData['rune']['type'] = runeSql.runeType
+                else
+                    runeData = apiGetStaticDataRune(slot['runeId'].to_s, key)
+                
+                    runeSql = Rune.new( :runeId => runeData['id'], :runeDescription => runeData['description'], :runeType => runeData['rune']['type'] )
+                    runeSql.save
+                end
+                @runes[index] = runeData['description']
+                #@runesColor[@runes[index]] = runeData['rune']['type']
+                case runeData['rune']['type']
+                when "red"
+                    @runesColor[@runes[index]] = runeData['rune']['type']
+                when "yellow"
+                    @runesColor[@runes[index]] = "#B8860B"
+                when "blue"
+                    @runesColor[@runes[index]] = runeData['rune']['type']
+                else
+                    @runesColor[@runes[index]] = "#800080"
+                end
+            end
+        end
+        
+        #sum all same rune
+        @runesSum = Hash.new
+        @runes.each do |rune|
+            if(@runesSum[rune] == nil)
+                @runesSum[rune] = 1
+            else
+                @runesSum[rune] += 1
+            end
+        end
+        
+        #find current masteries
+        parsedMasteries = apiGetSummonerMasteries(summonerId, key)
+        
+        @currentMasteryPage = -1
+        parsedMasteries[summonerId]['pages'].each_with_index  do |page, index|
+            if(page['current']==true)
+                @currentMasteryPage = index
+                break
+            end
+        end
+        
+        #current rank
+        @masteries = Hash.new
+        #total rank
+        @masteriesRank = Hash.new
+        @masteriesDescription = Hash.new
+        if((@currentMasteryPage != -1) && (!parsedMasteries[summonerId]['pages'][@currentMasteryPage]['masteries'].nil?))
+            parsedMasteries[summonerId]['pages'][@currentMasteryPage]['masteries'].each do |mastery|
+                @masteries[mastery['id']] = mastery['rank']
+            end
+        end
+        @masteriesNumber = 
+        [
+            [
+                [4111, 4112, 4113, 4114],
+                [4121, 4122, 4123, 4124],
+                [4131, 4132, 4133, 4134],
+                [4141, 4142, 4143, 4144],
+                [4151, 4152, 4153, 4154],
+                [4161, 4162, 4163, 4164],
+            ],
+            [
+                [4211, 4212, 4213, 4214],
+                [4221, 4222, 4223, 4224],
+                [4231, 4232, 4233, 4234],
+                [4241, 4242, 4243, 4244],
+                [4251, 4252, 4253, 4254],
+                [4261, 4262, 4263, 4264],
+            ],
+            [
+                [4311, 4312, 4313, 4314],
+                [4321, 4322, 4323, 4324],
+                [4331, 4332, 4333, 4334],
+                [4341, 4342, 4343, 4344],
+                [4351, 4352, 4353, 4354],
+                [4361, 4362, 4363, 4364],
+            ]
+        ]
+        @masteriesNumberNotExist = [4153, 4161, 4163, 4164, 4223, 4254, 4261, 4263, 4264, 4321, 4351, 4354, 4361, 4363, 4364]
+            
+        #get all masteriesDescription of masteries
+        @masteriesNumber.each do |i|
+            i.each do |j|
+                j.each do |k|
+                    if(!@masteriesNumberNotExist.include?(k))
+                        
+                        if(Mastery.where( :masteryId => k.to_s).exists?)
+                            masterySql = Mastery.where( :masteryId => k.to_s).first
+                            parsedMastery = Hash.new
+                            parsedMastery['ranks'] = masterySql.masteryRanks
+                            parsedMastery['id'] = masterySql.masteryId
+                            #.split(",") string(sql) to array
+                            parsedMastery['description'] = masterySql.masteryDescription.split(",")
+                            parsedMastery['name'] = masterySql.masteryName
+                        else
+                            parsedMastery = apiGetStaticDataMastery(k.to_s, key)
+                            #.join(",") array(json from api) to string
+                            masterySql = Mastery.new( :masteryRanks => parsedMastery['ranks'], :masteryId => parsedMastery['id'], :masteryDescription => parsedMastery['description'].join(","), :masteryName => parsedMastery['name'] )
+                            masterySql.save
+                        end
+                        
+                        #backup
+                        #parsedMastery = apiGetStaticDataMastery(k.to_s, key)
+                        
+                        if(@masteries.include?(k))
+                            @masteriesDescription[k] = parsedMastery['description'][@masteries[k].to_i-1]
+                            @masteriesRank[k] = @masteries[k].to_s + "\/" + parsedMastery['ranks'].to_s
+                        else
+                            @masteriesDescription[k] = parsedMastery['description']
+                            @masteriesRank[k] = "0\/" + parsedMastery['ranks'].to_s
+                        end
+                    end
+                end
+            end
+        end
     end
     def runepage
         key = "edadd4f4-b535-4c14-b2f8-b667e4a59e8c"
@@ -259,6 +423,7 @@ class LolController < ApplicationController
         #@runepage.save
         #redirect_to @runepage
         session[:guest] = params[:guest][:name].to_s.delete(' ').downcase
-        redirect_to '/lol/runepage'
+        #redirect_to '/lol/runepage'
+        redirect_to '/lol/show/'+session[:guest]
     end
 end
